97cd1ece9b080f120d44c79c84be65155625a8c1
nammba <pvitulano@gmail.com>
2022-10-30 08:00:24 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.A, function () {
     radio.sendString("I love you papa")
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("I love you mama and papa")
 })
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("I love you mama")
 })
 radio.setGroup(1)
